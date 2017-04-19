8169c848278dbd613f7cab7478fa58a9c6a307ab
ieee0824 <ieee0824@ezweb.ne.jp>
2017-04-19 11:31:54 +0900

    更新

--- thor.rb
+++ thor.rb
@@ -1,7 +1,7 @@
 class Thor < Formula
   desc "thor is docker deploy tool for ecs."
   homepage ""
-  url "https://github.com/jobtalk/thor/releases/download/v0.1.5/thor-darwin-amd64"
+  url "https://github.com/jobtalk/thor/releases/download/v0.1.6/thor-darwin-amd64"
   sha256 "32d8288c81e8947515ae7d40f82bee47d82ddeb40fb25771eb6e94d3f99d1085"
 
 
